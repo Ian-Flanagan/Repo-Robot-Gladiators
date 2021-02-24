718e615532a00ceb646f2588b55812ac884f4a19
Ian-Flanagan <77815095+Ian-Flanagan@users.noreply.github.com>
2021-02-23 19:34:35 -0600

    updating feature/initial-game branch, accidentally worked on the shop branch

--- assets/js/game.js
+++ assets/js/game.js
@@ -70,24 +70,24 @@ var fight = function(enemyName) {
 
 // function to start a new game
 var startGame = function() {
-    // reset player stats
-    playerHealth = 100;
-    playerAttack = 10;
-    playerMoney = 10;
-    for (var i = 0; i < enemyNames.length; i++) {
-        if (playerHealth > 0) {
-            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-  
-            var pickedEnemyName = enemyNames[i];
-  
-            enemyHealth = 50;
-  
-            fight(pickedEnemyName);
-        }
-        else {
-            window.alert("You have lost your robot in battle! Game Over!");
-            break;
-        }
+  // reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+  for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+      var pickedEnemyName = enemyNames[i];
+
+      enemyHealth = 50;
+
+      fight(pickedEnemyName);
+    }
+    else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+    }
         //after the loop ends, player is either out of health or enemies to fight, so run the endGame function
         endGame();
     }
@@ -115,6 +115,11 @@ var endGame = function() {
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
 };
+
+var shop = function() {
+  console.log("entered the shop");
+};
+
 
 //start the game when the page loads
 startGame();
